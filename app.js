f7cc5309e33ec0a2cca9b10f3e80469655842fb3
sagorsohel <sohel19sagor@gmail.com>
2024-10-23 23:55:09 +0600

    use error handler

--- app.js
+++ app.js
@@ -1,50 +1,48 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+// external imports
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
+//  internal imports
 
-const app = express()
-dotenv.config()
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
 
+const app = express();
+dotenv.config();
 
 // connect to the database
 
-mongoose.connect(process.env.MONGODB_URL,{
+mongoose
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(()=>{
-    console.log('MongoDB Connected...');
-})
-.catch(err => console.log(err));
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected...");
+  })
+  .catch((err) => console.log(err));
 
+// request parser
 
-// request parser 
-
-app.use(express.json())
-app.use(express.urlencoded())
-
-
+app.use(express.json());
+app.use(express.urlencoded());
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
-
-
-// routing 
-
+// routing
 
 // error handler
-
-
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
-
-
-
-
+app.use(errorHandler)
+app.use(errorHandler)
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
