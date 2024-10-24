6a6af85ac2b38936e0e5fce2775fccfa2615b867
sagorsohel <sohel19sagor@gmail.com>
2024-10-24 23:52:25 +0600

    full setup common error

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -1,25 +1,31 @@
-
-const createError=require('http-errors')
+const createError = require("http-errors");
 
 function notFoundHandler(req, res, next) {
-  next(createError(404,'Request not found!'))
+  next(createError(404, "Request not found!"));
 }
 
 function errorHandler(err, req, res, next) {
-    res.render('error',{
-        title:'Error page'
-    })
-    // res.status(err.status || 500)
-    // res.json({
-    //     error: {
-    //         message: err.message,
-    //         stack: err.stack
-    //     }
-    // })
-   
+  res.locals.error =
+    process.env.NODE_ENV !== "production" ? err : { message: err.message };
+
+  res.status(err.status || 500);
+  if (res.locals.html) {
+    res.render("error", {
+      title: "Error page",
+    });
+  } else {
+    res.json(res.locals.error);
+  }
+  // res.status(err.status || 500)
+  // res.json({
+  //     error: {
+  //         message: err.message,
+  //         stack: err.stack
+  //     }
+  // })
 }
 
-module.exports={
-    notFoundHandler,
-    errorHandler
-}
+module.exports = {
+  notFoundHandler,
+  errorHandler,
+};
